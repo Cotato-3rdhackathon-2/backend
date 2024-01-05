88ccd5b7d1c0ac9d1ded1e145db2c7dca8700acf
qjvk2880 <qjvk2880@gmail.com>
2024-01-05 20:02:43 +0900

    Initial Commit

---
+++ src/test/java/com/example/farewell/FarewellApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.farewell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarewellApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
